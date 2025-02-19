cfa544e060cfc9f0c9c5f9aa674f0f8066b342f9
Guilherme Oliveira <gui.live012@gmail.com>
2025-02-19 19:46:41 -0300

    contruindo a estrutura e a seção "sobre mim"

---
+++ src/containers/Sidebar/index.tsx
@@ -0,0 +1,10 @@
+import Titulo from '../../components/Titulo'
+
+const Sidebar = () => (
+  <aside>
+    <img src="https://github.com/srguioliveira.png" alt="" />
+    <Titulo fontSize={20}>Guilherme Oliveira</Titulo>
+  </aside>
+)
+
+export default Sidebar
---
+++ src/containers/Sobre/index.tsx
@@ -0,0 +1,21 @@
+import Titulo from '../../components/Titulo'
+import Paragrafo from '../../components/Paragrafo'
+import { GithubSecao } from './styles'
+
+const Sobre = () => (
+  <section>
+    <Titulo fontSize={16}>Sobre mim</Titulo>
+
+    <Paragrafo tipo="secundario">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi, voluptatum
+      beatae. Officia deserunt amet, minus suscipit vel culpa eos explicabo quam
+      recusandae consequuntur iure inventore illo quisquam est perspiciatis in?
+    </Paragrafo>
+    <GithubSecao>
+      <img src="https://github-readme-stats.vercel.app/api?username=srguioliveira&show_icons=true&theme=dracula&include_all_commits=true&count_private=true" />
+      <img src="https://github-readme-stats.vercel.app/api/top-langs/?username=srguioliveira&layout=compact&langs_count=7&theme=dracula" />
+    </GithubSecao>
+  </section>
+)
+
+export default Sobre
