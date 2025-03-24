9b6722e67db1caca865d722c876493861bcc3ced
Guilherme Oliveira <gui.live012@gmail.com>
2025-03-23 21:00:56 -0300

    atualização da Secão Sobre Mim

--- src/containers/Sobre/index.tsx
+++ src/containers/Sobre/index.tsx
@@ -7,9 +7,24 @@ const Sobre = () => (
     <Titulo fontSize={16}>Sobre mim</Titulo>
 
     <Paragrafo $tipo="secundario">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi, voluptatum
-      beatae. Officia deserunt amet, minus suscipit vel culpa eos explicabo quam
-      recusandae consequuntur iure inventore illo quisquam est perspiciatis in?
+      Sou um desenvolvedor front-end júnior, apaixonado por tecnologia e em
+      constante aprendizado. Este projeto foi desenvolvido como uma forma de
+      demonstrar minhas habilidades técnicas e meu potencial na área. Domino{' '}
+      <strong>HTML</strong>, <strong>CSS</strong>, <strong>JavaScript</strong>,{' '}
+      <strong>TypeScript</strong>, <strong>Git</strong>, <strong>Sass</strong>,{' '}
+      <strong>jQuery</strong>, <strong>Bootstrap</strong>,{' '}
+      <strong>React</strong> e <strong>Redux</strong>, sempre com foco em criar
+      aplicações que sejam responsivas, acessíveis e otimizadas para diferentes
+      dispositivos. Tenho um forte compromisso com boas práticas de
+      desenvolvimento e organização de código. Minha motivação como
+      desenvolvedor é criar interfaces que sejam intuitivas e impactantes,
+      unindo funcionalidade e design. Durante o desenvolvimento deste projeto,
+      busquei integrar tecnologias modernas e explorar formas criativas de
+      solucionar desafios. Embora ainda esteja iniciando minha trajetória
+      profissional, este projeto reflete minha dedicação, curiosidade e
+      capacidade de entregar soluções reais. Meu objetivo é continuar aprendendo
+      e contribuir para projetos desafiadores que tragam valor aos usuários e ao
+      mercado.{' '}
     </Paragrafo>
     <GithubSecao>
       <img src="https://github-readme-stats.vercel.app/api?username=srguioliveira&show_icons=true&theme=dracula&include_all_commits=true&count_private=true" />
